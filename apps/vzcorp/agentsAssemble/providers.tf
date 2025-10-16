edc7f670df559947b66ac296d1dc216a5cfa6e7f
vzisiadis <vzisiadis@gmail.com>
2025-10-16 19:17:31 +0300

    azurerm updated

--- apps/vzcorp/agentsAssemble/providers.tf
+++ apps/vzcorp/agentsAssemble/providers.tf
@@ -7,20 +7,8 @@ terraform {
       version = ">= 3.111.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "sttfstatevzcorp"
-    container_name       = "tfstate"
-    key                  = "vzcorp-agentsAssemble.tfstate"
-  }
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-  storage_use_azuread = true
+  features {}
 }
